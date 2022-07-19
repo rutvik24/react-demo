b1bb4d6b6613da6f57423541bb5decf432fe8dcc
rutvik24 <rutviknabhoya2001@gmail.com>
2022-07-19 12:31:10 +0530

    environment

---
+++ env.config/env.config.js
@@ -0,0 +1,44 @@
+const { spawn } = require('child_process');
+const shell = require('shelljs');
+
+const config = {
+  stage: '.envStg',
+  prod: '.envProd',
+  test: '.envTest',
+  develop: '.envDev',
+};
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise(resolve => {
+    spawned.stdout.on('data', data => {
+      const response = data.toString();
+      resolve(response);
+    });
+
+    spawned.stderr.on('data', data => {
+      console.error('err', data.toString());
+    });
+
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
+
+const main = async () => {
+  const currentBranch = (
+    await runCommand('git', ['branch', '--show-current'])
+  ).trim();
+  console.log(currentBranch);
+  let env = config[currentBranch];
+  if (!env) {
+    env = config.develop;
+  }
+  console.log(env);
+  const path = `./env.config/${env}`;
+  shell.cp(path, './.env');
+};
+
+main();
