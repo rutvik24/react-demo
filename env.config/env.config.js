9a0989105e30f16319f3fda1a53d49ae9bc5890f
rutvik24 <rutviknabhoya2001@gmail.com>
2022-07-19 14:00:58 +0530

    environment

--- env.config/env.config.js
+++ env.config/env.config.js
@@ -8,27 +8,8 @@ const config = {
   develop: ".envDev",
 };
 
-function runCommand(command, args, options = undefined) {
-  const spawned = spawn(command, args, options);
-
-  return new Promise((resolve) => {
-    spawned.stdout.on("data", (data) => {
-      const response = data.toString();
-      resolve(response);
-    });
-
-    spawned.stderr.on("data", (data) => {
-      console.error("err", data.toString());
-    });
-
-    spawned.on("close", () => {
-      resolve();
-    });
-  });
-}
-
 const main = async () => {
-  const currentBranch = "prod";
+  const currentBranch = "develop";
   console.log(currentBranch);
   let env = config[currentBranch];
   if (!env) {
