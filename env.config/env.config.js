0150b2e953cd7cc93397bcd6b74de3a4d6683e6a
rutvik24 <rutviknabhoya2001@gmail.com>
2022-07-19 14:02:01 +0530

    stage

--- env.config/env.config.js
+++ env.config/env.config.js
@@ -9,7 +9,7 @@ const config = {
 };
 
 const main = async () => {
-  const currentBranch = "develop";
+  const currentBranch = "stage";
   console.log(currentBranch);
   let env = config[currentBranch];
   if (!env) {
